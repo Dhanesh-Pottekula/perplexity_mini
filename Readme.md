d5fcba243a2e8fada23f76c90ce6f9ffada1e64c
dhanesh <dhanesh@airaai.com>
2025-09-19 21:34:49 +0530

    Add PM2 for process management, update README with usage instructions, and simplify server setup

---
+++ Readme.md
@@ -0,0 +1,15 @@
+run docker-compose up before starting teh app 
+
+
+start the server -npm start 
+
+start server with workers mentinod and debugger pm2 start server.js -i 1 --node-args="--inspect=9229"
+
+list of server pm2 list 
+
+stop server pm2 stop <server_name> 
+
+stop server by id pm2 stop <id>
+
+stop all servers   pm2 stop all 
+
--- server.js
+++ server.js
@@ -1,26 +1,9 @@
-import cluster from "cluster";
-import { cpus } from "os";
 import { createApp } from "./app.js";
 
-const numCPUs = cpus().length;
-
-if (cluster.isPrimary) {
-  console.log(`🚀 Master ${process.pid} running with ${numCPUs} workers`);
-
-  for (let i = 0; i < numCPUs; i++) {
-    cluster.fork();
-  }
-
-  cluster.on("exit", (worker) => {
-    console.log(`❌ Worker ${worker.process.pid} died, restarting...`);
-    cluster.fork();
-  });
-} else {
-  (async () => {
-    const app = await createApp();
-    const PORT = process.env.PORT || 3000;
-    app.listen(PORT, () =>
-      console.log(`⚡ Worker ${process.pid} running on port ${PORT}`)
-    );
-  })();
-}
+(async () => {
+  const app = await createApp();
+  const PORT = process.env.PORT || 3000;
+  app.listen(PORT, () =>
+    console.log(`⚡ Server running on port ${PORT}`)
+  );
+})();
